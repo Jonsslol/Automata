cec13bf7fc265e47c840b98c7a44bca6d6406225
Jonsslol <jvquadros2010@hotmail.com>
2021-09-06 20:54:44 -0300

    Automação do zap

---
+++ main.py
@@ -0,0 +1,39 @@
+# Passo a passo da automatização...
+import pyautogui
+import time
+# Exportar os aquivos do ecxel direto do Nuubes
+pyautogui.position()
+pyautogui.alert('Começou o precesso. Não toque no computador')
+pyautogui.PAUSE = 1
+pyautogui.press("winleft")
+pyautogui.write('Microsoft edge')
+pyautogui.press('enter')
+pyautogui.write('https://web.whatsapp.com/')
+pyautogui.press('enter')
+time.sleep(3)
+pyautogui.moveTo(188,250)
+time.sleep(1.5)
+pyautogui.mouseDown()
+pyautogui.write('testando...')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.moveTo(502,142)
+pyautogui.doubleClick()
+time.sleep(1)
+pyautogui.moveTo(1155,249)
+time.sleep(1)
+
+
+
+
+
+
+
+
+# Formatar esse arquivos para se tornarem legiveis ao leitor
+
+# Extrarir esses arquivos para PDF
+
+# Levar cada arquivo PDF para sua pasta dentro do FTP
+
+
